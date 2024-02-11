29e5c32c218fb2250f7f8dba60e61b02c182b189
veddahit <95873386+veddahit@users.noreply.github.com>
2024-02-12 01:18:36 +1100

    final

--- qn2.py
+++ qn2.py
@@ -1,9 +1,12 @@
+
+# git link    https://github.com/veddahit/SoftwareNow_Assigmnet_3
+
 import pygame
 pygame.init()
 
 win = pygame.display.set_mode((500,480))
 
-pygame.display.set_caption("First Game")
+pygame.display.set_caption("Slider Game")
 
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
